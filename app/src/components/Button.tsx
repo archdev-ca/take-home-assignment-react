d94248a3312647306a619f915004a43d8499fe6b
acgelonga <acgelonga@gmail.com>
2023-11-01 20:07:20 -0500

    Finished Button styling and added a bunch of custom colors

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -9,16 +9,20 @@ type Props = {
 }
 
 const variantClassMap = {
-    primary: 'bg-primary',
+    primary: 'bg-primary-main hover:bg-primary-dark active:bg-primary-darker focus:ring-4 focus:ring-primary-lighter',
 }
 
 const Button = ({ variant, fullWidth, children }: Props) => {
     let classNames = ''
-    classNames += variantClassMap[variant] ? variantClassMap[variant] : 'bg-primary'
+    classNames += variantClassMap[variant] ? variantClassMap[variant] : 'bg-primary-main'
     classNames += fullWidth ? ' w-full block' : ''
     return (
         <>
-            <button className={`${classNames} rounded-sm text-white leading-6 py-2`}>{children}</button>
+            <button
+                className={`${classNames} rounded-sm text-primary-contrast leading-6 py-2 transition duration-300 ease-in-out`}
+            >
+                {children}
+            </button>
         </>
     )
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,tsx,jsx}"
-  ],
-  theme: {
-    extend: {
-      borderRadius: {
-        'large': '10px',
-      },
-      colors: {
-      'primary': '#9A48D6'
+    content: ['./index.html', './src/**/*.{js,ts,tsx,jsx}'],
+    theme: {
+        extend: {
+            borderRadius: {
+                large: '10px',
+            },
+            colors: {
+                primary: {
+                    main: '#9A48D6',
+                    dark: '#803bb3',
+                    darker: '#662f8f',
+                    light: '#a85fde',
+                    lighter: '#c296e3',
+                    contrast: '#fff',
+                },
+            },
+        },
     },
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
-
