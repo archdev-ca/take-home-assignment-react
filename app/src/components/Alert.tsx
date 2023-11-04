592058500e01ed3219247c1a0e8182d637c5685b
acgelonga <acgelonga@gmail.com>
2023-11-03 20:56:26 -0500

    Updated alert styling

--- app/src/components/Alert.tsx
+++ app/src/components/Alert.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 type Props = {
     variant: 'error' | 'success'
     children: React.ReactNode
-    classNames: string
+    classNames?: string
 }
 
 function Alert({ variant, classNames, children }: Props) {
     let classes = ''
     if (variant === 'error') {
-        classes += 'bg-red-200 text-red-700 border-red-500'
+        classes += 'bg-red-100 text-red-700 border-red-500'
     }
     if (variant === 'success') {
-        classes += 'bg-green-200 text-green-700 border-green-500'
+        classes += 'bg-green-100 text-green-700 border-green-500'
     }
-    return <div className={`${classNames} ${classes} rounded-sm p-4 border-l-6 `}>{children}</div>
+    return <div className={`${classNames} ${classes} rounded-sm p-4 border-l-4 `}>{children}</div>
 }
 
 export default Alert
