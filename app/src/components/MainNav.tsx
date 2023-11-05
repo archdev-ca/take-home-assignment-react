42cf0aef9eb53a89fab92a16c2cf180841c06987
acgelonga <acgelonga@gmail.com>
2023-11-05 16:44:24 -0600

    Check for auth and display either signin or logout button

--- app/src/components/MainNav.tsx
+++ app/src/components/MainNav.tsx
@@ -1,15 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Button from './Button'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 function MainNav() {
+    const navigate = useNavigate()
+    const { accessToken, refreshToken } = useContext(AppContext)
+
+    const handleSignin = () => {
+        navigate('/login')
+    }
+
+    const handleLogout = () => {}
+
     return (
-        <nav className="border-b border-gray-300 py-4 px-8 flex flex-row place-content-between">
+        <nav className="border-b border-gray-300 py-4 px-8 flex flex-row">
             <Link to="/">
                 <img src="/images/logo.png" srcSet="/images/logo@2x.png 2x" alt="" width={143} height={24} />
             </Link>
-            <Button variant="contained" color="primary">
-                Sign in
-            </Button>
+            {!accessToken || !refreshToken ? (
+                <Button classNames="ml-auto" variant="contained" color="primary" onClick={handleSignin}>
+                    Sign in
+                </Button>
+            ) : null}
+            {accessToken && refreshToken ? (
+                <Button classNames="ml-auto" variant="contained" color="primary" onClick={handleLogout}>
+                    Logout
+                </Button>
+            ) : null}
         </nav>
     )
 }
