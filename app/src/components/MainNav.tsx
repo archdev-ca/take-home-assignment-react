2fd74e5584a6eb2b674845ffe5c507d802d0a24b
acgelonga <acgelonga@gmail.com>
2023-11-05 16:05:58 -0600

    Added link to logo

--- app/src/components/MainNav.tsx
+++ app/src/components/MainNav.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom'
 import Button from './Button'
 
 function MainNav() {
     return (
         <nav className="border-b border-gray-300 py-4 px-8 flex flex-row place-content-between">
-            <img src="/images/logo.png" srcSet="/images/logo@2x.png 2x" alt="" width={143} height={24} />
-            <Button color="primary">Sign in</Button>
+            <Link to="/">
+                <img src="/images/logo.png" srcSet="/images/logo@2x.png 2x" alt="" width={143} height={24} />
+            </Link>
+            <Button variant="contained" color="primary">
+                Sign in
+            </Button>
         </nav>
     )
 }
