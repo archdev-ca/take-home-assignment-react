579af90d18553c806cc0daef04583b2c80cdd846
acgelonga <acgelonga@gmail.com>
2023-11-06 01:04:52 -0600

    Use apollo links to refres/retry tokens

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,16 +3,84 @@ import './styles/global.css'
 import Home from './routes'
 import Login from './routes/login'
 import Products from './routes/products'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, from, Observable } from '@apollo/client'
 import { useContext, useEffect } from 'react'
 import { AppContext } from './context/AppContext'
+import { onError } from '@apollo/client/link/error'
+import { setContext } from '@apollo/client/link/context'
+import { REFRESH_TOKEN } from './gql/mutations/auth'
+import { GraphQLError } from 'graphql'
 
 const GRAPHQL_URL = 'http://localhost:8080/graphql'
+
+const getOperationAuthHeader = (operation: string | undefined) => {
+    return operation !== 'RefreshTokenMutation' ? { authorization: `Bearer ${localStorage.getItem('at')}` } : {}
+}
+
+const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+        for (const err of graphQLErrors) {
+            switch (err.extensions.code) {
+                case 'SESSION_EXPIRED':
+                    if (operation?.operationName === 'RefreshTokenMutation') return
+                    renewToken()
+
+                    return new Observable(observer => {
+                        ;(async () => {
+                            try {
+                                const { accessToken, refreshToken } = await renewToken()
+                                if (!accessToken || !refreshToken) {
+                                    throw new GraphQLError('empty token')
+                                }
+
+                                const subscriber = {
+                                    next: observer.next.bind(observer),
+                                    error: observer.error.bind(observer),
+                                    complete: observer.complete.bind(observer),
+                                }
+                                forward(operation).subscribe(subscriber)
+                            } catch (err) {
+                                observer.error(err)
+                            }
+                        })()
+                    })
+            }
+        }
+    }
+    forward(operation)
+})
+
+const httpLink = createHttpLink({
+    uri: GRAPHQL_URL,
+})
+
+const authLink = setContext((operation, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            ...getOperationAuthHeader(operation.operationName),
+        },
+    }
+})
 
 const client = new ApolloClient({
     uri: GRAPHQL_URL,
     cache: new InMemoryCache(),
+    link: from([errorLink, authLink, httpLink]),
 })
+
+const renewToken = async () => {
+    const response = await client.mutate({
+        mutation: REFRESH_TOKEN,
+        variables: {
+            accessToken: localStorage.getItem('at'),
+            refreshToken: localStorage.getItem('rt'),
+        },
+    })
+    localStorage.setItem('at', response.data.refreshSession.accessToken)
+    localStorage.setItem('rt', response.data.refreshSession.refreshToken)
+    return response.data.refreshSession
+}
 
 const ProtectedRoute = () => {
     const navigate = useNavigate()
--- app/src/hooks/useProductList.ts
+++ app/src/hooks/useProductList.ts
@@ -1,35 +1,8 @@
-import { ServerError, useLazyQuery } from '@apollo/client'
+import { useLazyQuery } from '@apollo/client'
 import { PRODUCT_LIST } from '../gql/queries/product'
-import { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
-import { GQLServerErrorInterface } from '../interfaces/common'
-import useRenewToken from './useRenewToken'
 
 function useProductList() {
-    const { accessToken, refreshToken } = useContext(AppContext)
-    const { renewToken } = useRenewToken()
-    const [getProductList, { loading, error, data }] = useLazyQuery(PRODUCT_LIST, {
-        onError: error => {
-            if (error && error.networkError?.name && error.networkError?.name === 'ServerError') {
-                ;((error.networkError as ServerError).result as GQLServerErrorInterface).errors.every(err => {
-                    if (err.extensions.code === 'SESSION_EXPIRED') {
-                        renewToken({
-                            variables: {
-                                accessToken,
-                                refreshToken,
-                            },
-                        })
-                        return
-                    }
-                })
-            }
-        },
-        context: {
-            headers: {
-                authorization: `${accessToken ? `Bearer ${accessToken}` : ''}`,
-            },
-        },
-    })
+    const [getProductList, { loading, error, data }] = useLazyQuery(PRODUCT_LIST, {})
     return {
         getProductList,
         loading,
